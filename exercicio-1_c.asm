8e48a2e408ff19ec9a203755789fbb8a404aae8d
Filiphe Vilar Figueiredo <liphvf@gmail.com>
2018-04-08 14:58:38 -0300

    organizando

---
+++ exercicio-1_c.asm
@@ -0,0 +1,30 @@
+.data
+	a: .word 10
+.text
+	lw $t1, a # Carregando a
+	li $t2, 0 # Declarando b
+	
+	blt $t1, $zero, else
+	
+	addi $t2, $t1, 10
+	
+	li $v0, 1 # preparando para print
+	
+	add $a0, $t2, $zero # Copiando valor de t2 para a0
+	
+	syscall # chama o print
+	
+	li $v0, 10 # exit program
+	
+	syscall # chama o exit
+	
+	else:
+		addi $t2, $t1, -10
+	
+		li $v0, 1 # preparando para print
+	
+		add $a0, $t2, $zero # Copiando valor de t2 para a0
+	
+		syscall
+		
+		
