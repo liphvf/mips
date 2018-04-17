0b19e9f8905dceb3944c813b3d1b60177e42fb2f
Filiphe Vilar Figueiredo <liphvf@gmail.com>
2018-04-17 15:05:24 -0300

    remove (#3)

--- mips1.asm
+++
@@ -1,6 +0,0 @@
-.data 
-	nome: .asciiz  "Filiphe Vilasr Figueiredo" 
-.text
-	li $v0, 4 # Estou carregando o valor quatro em $v0 para imprimir nome
-	la $a0, nome # diz o que vai ser printado
-	syscall # chama o sisterma para printar
